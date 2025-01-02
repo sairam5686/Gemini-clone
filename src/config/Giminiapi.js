367177d3e170e32d266b210c696cdc0bfe6113ad
Sairam <jrsairam5686@gmail.com>
2025-01-03 01:31:32 +0530

    --modified commit -geminiapi. -integrate -n

---
+++ src/config/Giminiapi.js
@@ -0,0 +1,36 @@
+//AIzaSyC8CoBuxgEOp7eMzBsTrayNZhC6ebHQ2ZE
+
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+  
+  const apiKey = "AIzaSyC8CoBuxgEOp7eMzBsTrayNZhC6ebHQ2ZE";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash-exp",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(propt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+  }
+  
+  export default run;
