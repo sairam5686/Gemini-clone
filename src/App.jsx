cd23e2adaa0ae95b7a9ed7104a812d26ce175e00
Sairam <jrsairam5686@gmail.com>
2025-01-02 22:57:38 +0530

    initial --commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import  Imp from './components/main/Imp'
+import Navbar from './components/navbar/Navbar';
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Imp />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/main/Imp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Imp = () => {
+  return (
+    <div>Imp</div>
+  )
+}
+
+export default Imp
