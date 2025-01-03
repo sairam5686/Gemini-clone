24a6e062f0713d9460555006cd5f2837e96d83c6
sairam5686 <jrsairam5686@gmail.com>
2025-01-03 21:56:05 +0530

    -functionality -complete commit

--- src/components/main/Imp.jsx
+++ src/components/main/Imp.jsx
@@ -1,26 +1,139 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Imp.css'
 import { PiCompassRoseLight } from "react-icons/pi";
 import { IoBulbOutline } from "react-icons/io5";
 import { CiChat1 } from "react-icons/ci";
 import { FaCode } from "react-icons/fa";
-
-
+import { Geminicontext } from '../../context/Api';
 
 
 
 const Imp = () => {
-  return (
-    <div className='main-parent flex-1 flex flex-col items-center my-3 '>
 
-    <div className="block_1s pt-5  p-9 ">
+  const {Request, setRequest, response, setResponse,Skeleton, Pressed, setPressed, prompt_logic} = useContext(Geminicontext);
+
+
+  return (
+
+   
+
+      
+ <div className='main-parent flex-1 flex flex-col items-center my-3 '>
+
+  {
+    Pressed ? <>
+  
+      <div className="block_3s mt-5"> 
+
+          <div className="messageBox">
+            <input  placeholder="Enter a prompt here ..." value={Request} onChange={(e)=>{setRequest(e.target.value); }}  type="text" id="messageInput" />
+            <button id="sendButton" onClick={()=>{
+              setPressed(true);
+              prompt_logic(Request);
+            }}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 664 663">
+                <path
+                  fill="none"
+                  d="M646.293 331.888L17.7538 17.6187L155.245 331.888M646.293 331.888L17.753 646.157L155.245 331.888M646.293 331.888L318.735 330.228L155.245 331.888"
+                ></path>
+                <path
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  stroke-width="33.67"
+                  stroke="#6c6c6c"
+                  d="M646.293 331.888L17.7538 17.6187L155.245 331.888M646.293 331.888L17.753 646.157L155.245 331.888M646.293 331.888L318.735 330.228L155.245 331.888"
+                ></path>
+              </svg>
+            </button>
+          </div>
+      </div>
+
+      <div className="block_4s">
+        {Skeleton ?
+        <>
+        <div className='mt-20 flex flex-col gap-6'>
+                <div class="flex items-center w-full ">
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+                    <div class="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+                </div>
+                <div class="flex items-center w-full ">
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                    <div class="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-3/4"></div>
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                </div>
+                <div class="flex items-center w-full ">
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                    <div class="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-3/4"></div>
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                </div>
+                <div class="flex items-center w-full ">
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                    <div class="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-3/4"></div>
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                </div>
+                <div class="flex items-center w-full ">
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                    <div class="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-3/4"></div>
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                </div>
+                <div class="flex items-center w-full ">
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-3/4"></div>
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-3/4"></div>
+                    <div class="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+                </div>
+                <div class="flex items-center w-full ">
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                    <div class="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                </div>
+                <div class="flex items-center w-full ">
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                    <div class="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                </div>
+                <div class="flex items-center w-full ">
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+                    <div class="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+                </div>
+                <div class="flex items-center w-full ">
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                    <div class="h-2.5 ms-2 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+                    <div class="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+                </div>
+                <span class="sr-only">Loading...</span>
+                </div>
+          
+      
+        </> 
+        : 
+        <>
+        <div className='result'>
+         <div className='resultbox'>
+          <p >{response}</p>
+        </div>
+        </div>
+        
+        </>
+        
+        }
+       
+
+      </div>
+    </>
+    
+    
+    :
+    
+    
+    <>
+     <div className="block_1s pt-5  p-9 ">
       <h1 className='text-6xl font-semibold leading-normal'>
         <span>Hello, Dev.</span>
       </h1>
       <h1 className=' text-6xl font-semibold'>How can I help you today?</h1>
     </div>
-
-
 
       <div className="block_2s mt-3  ">
         <div className="card bg-zinc-800 rounded-lg flex flex-row justify-between gap-8 flex-wrap p-4">
@@ -49,8 +162,11 @@ const Imp = () => {
       <div className="block_3s mt-20"> 
 
           <div className="messageBox">
-            <input required="" placeholder="Enter a prompt here ..." type="text" id="messageInput" />
-            <button id="sendButton">
+            <input  placeholder="Enter a prompt here ..." value={Request} onChange={(e)=>{setRequest(e.target.value); }}  type="text" id="messageInput" />
+            <button id="sendButton" onClick={()=>{
+              setPressed(true);
+              prompt_logic(Request);
+            }}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 664 663">
                 <path
                   fill="none"
@@ -69,6 +185,10 @@ const Imp = () => {
 
 
       </div>
+    </> 
+  }
+  
+   
 
     </div>
   )
--- src/context/Api.jsx
+++ src/context/Api.jsx
@@ -1,17 +1,31 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 import run from '../config/Giminiapi';
 
 export const Geminicontext = createContext();
 
 const Api = ({children}) => {
+   // for all the state variables
+    
+   const [Request, setRequest] = useState("");
+   const [response, setResponse] = useState("");
+   const [Pressed, setPressed] = useState(false);
+   const [Skeleton, setSkeleton] = useState(false);
+
 
     const prompt_logic = async (prompt) => {
-        await run(prompt)
+      setSkeleton(true);
+      setPressed(true);
+      const responsee =  await run(prompt);
+      setSkeleton(false);
+      setResponse(responsee);
     }
-    
 
+
+
+    // for the context datas and func
 
     const Contextdata ={
+      Request, setRequest, response, setResponse, Pressed, setPressed, prompt_logic,Skeleton
 
     }
 
